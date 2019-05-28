e6d12dd455eb4b3ef3edb04b6275bdbe77957528
MichaelJahns <jahnsmic001@gmail.com>
2019-05-28 14:33:54 -0700

    Code Review; fixes shared preferences and pulls DB data for stream

--- app/src/main/java/com/example/healthtracker/streamEntryActivity.java
+++ app/src/main/java/com/example/healthtracker/streamEntryActivity.java
@@ -3,22 +3,34 @@ package com.example.healthtracker;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class streamEntryActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     EntryLayoutAdapter adapter;
+    EntryDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stream_entry);
 
-        ArrayList<Entry> entries = new ArrayList<>();
+        database = Room.databaseBuilder(
+                getApplicationContext(),
+                EntryDatabase.class,
+                "entry-db")
+                .allowMainThreadQueries()
+                .fallbackToDestructiveMigration()
+                .build();
+
+        List<Entry> entries = database.entryDAO().getAll();
+
         entries.add(new Entry("Farted while runnin", "5:5", 3));
         entries.add(new Entry("Ran really far", "23:12", 4));
         entries.add(new Entry("Ow", "5:5", 1));
